050e513006bf1b616b81bf8049098cc8d6bb17da
Chun Shen <chunshen@wayne.edu>
2024-10-04 09:27:40 -0400

    add color scheme

--- nvim/lua/vim-options.lua
+++ nvim/lua/vim-options.lua
@@ -7,6 +7,12 @@ vim.cmd("set listchars=tab:>-,trail:·")
 vim.cmd("set colorcolumn=80")
 vim.g.mapleader = ";"
 
+vim.o.autoread = true
+vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "CursorHoldI", "FocusGained" }, {
+  command = "if mode() != 'c' | checktime | endif",
+  pattern = { "*" },
+})
+
 -- Navigate vim panes better
 vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
 vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
